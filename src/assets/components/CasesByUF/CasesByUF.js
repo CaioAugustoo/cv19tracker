618354c4376a61a34bd3122773f8e68032515037
Caio Augusto <62035389+CaioAugustoo@users.noreply.github.com>
2020-10-07 13:59:21 -0300

    Uso de Hooks

--- src/assets/components/CasesByUF/CasesByUF.js
+++ src/assets/components/CasesByUF/CasesByUF.js
@@ -1,67 +1,48 @@
-import React from 'react'
-import './styles/css/style.css';
+import React, { useState, useEffect } from "react";
+import "./styles/css/style.css";
 
-class CasesByUF extends React.Component {
-  state = {
-    uf: [],
-    uid: 0,
-  };
+const CasesByUF = () => {
+  const [uf, setUf] = useState(null);
 
-  async componentDidMount() { 
-    const url = 'https://covid19-brazil-api.now.sh/api/report/v1';
-    const response = await fetch(url);
-    const body = await response.json();
-    this.setState({
-      uf: body.data,
-      uid: body.data.uid
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://covid19-brazil-api.now.sh/api/report/v1"
+      );
+      const json = await response.json();
+      setUf(json.data);
+    };
+    fetchData();
+  }, []);
+  if (uf === null) return null;
+  return (
+    <section id="casesByStates">
+      <h1>Casos no Brasil por estados</h1>
+      <div id="tableCases">
+        <div
+          id="data"
+          className="d-flex justify-content-between mb-2 mt-4 pb-2"
+        >
+          <p>Estado</p>
+          <p>Confirmados</p>
+          <p>Óbitos</p>
+        </div>
 
-  };
-
-  render() {
-    const { uf } = this.state;
-    return (
-      <section 
-        id="casesByStates">
-        <h1>Casos no Brasil por estados</h1>
-      <div 
-        id="tableCases">
-          <div
-            id="data" 
-            className="d-flex justify-content-between mb-2 mt-4 pb-2"> 
-            <p>
-             Estado
-            </p>
-            <p>
-              Confirmados
-            </p>
-            <p>
-              Óbitos
-            </p>
-          </div>
-          <div className="mb-2"> 
-            {uf.map(uf => (
-              <div 
-                className="d-flex justify-content-between  align-items-center infos"
-                 key={uf.uid}
-                 >
-                <p 
-                  className="py-1">
-                  {uf.uf}
-                </p>
-                <p 
-                  {uf.cases}
-                </p>
-                <p>
-                  {uf.deaths}
-                </p>
-              </div>       
-            ))}
-          </div>
-        </div> 
-      </section>
-    );
-  };
+        <div className="mb-2">
+          {uf.map((uf) => (
+            <div
+              className="d-flex justify-content-between align-items-center infos"
+              key={uf.uid}
+            >
+              <p className="py-1">{uf.uf}</p>
+              <p>{uf.cases}</p>
+              <p>{uf.deaths}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default CasesByUF;
