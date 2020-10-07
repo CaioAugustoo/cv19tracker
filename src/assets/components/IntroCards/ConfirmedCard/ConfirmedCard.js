2b71c54ca5f3ab648a525be10161ee2ccbf7aeec
Caio Augusto <62035389+CaioAugustoo@users.noreply.github.com>
2020-10-07 14:07:56 -0300

    Uso de Hooks

--- src/assets/components/IntroCards/ConfirmedCard/ConfirmedCard.js
+++ src/assets/components/IntroCards/ConfirmedCard/ConfirmedCard.js
@@ -1,41 +1,33 @@
-import React from 'react'
-import './styles/css/styles.css';
+import React, { useEffect, useState } from "react";
+import "./styles/css/styles.css";
 
-export default class FetchData extends React.Component {
-  state = {
-    confirmed: null,
-  };
+const FetchData = () => {
+  const [confirmed, setConfirmed] = useState(null);
 
-  async componentDidMount() {
-    const url = 'https://api.covid19api.com/world/total';
-    const response = await fetch(url);
-    const body = await response.json();
-    this.setState({ confirmed: body.TotalConfirmed })
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("https://api.covid19api.com/world/total");
+      const json = await response.json();
+      setConfirmed(json.TotalConfirmed);
+    };
+    fetchData();
+  }, []);
 
-  render() {
-    return (
-      <section>
-        <div 
-          id="confirmed_box" 
-          className="d-flex align-items-center justify-content-center" 
-          style={{'marginRight': '30px'}} 
-          data-tip="Casos totais confirmados"
-          data-place="right"
-          >
-          <div>
-            <h6 
-              style={{fontWeight: '400'}}>
-              Casos confirmados
-            </h6>
+  return (
+    <section>
+      <div
+        id="confirmed_box"
+        className="d-flex align-items-center justify-content-center"
+        style={{ marginRight: "30px" }}
+      >
+        <div>
+          <h6 style={{ fontWeight: "400" }}>Casos confirmados</h6>
 
-            <h5 
-              style={{fontWeight: '400'}}>
-              {this.state.confirmed}
-            </h5>
-          </div>
+          <h5 style={{ fontWeight: "400" }}>{confirmed}</h5>
         </div>
-      </section>
-    );
-  };
-}
+      </div>
+    </section>
+  );
+};
+
+export default FetchData;
