de4fc622478d586de4f8844a2962f63b579775b0
Caio Augusto <62035389+CaioAugustoo@users.noreply.github.com>
2020-10-07 13:52:11 -0300

    Uso de Hooks

--- src/assets/components/CardsByCountry/CardsGeneral/CasesChina/CasesChina.js
+++ src/assets/components/CardsByCountry/CardsGeneral/CasesChina/CasesChina.js
@@ -1,55 +1,46 @@
-import React from 'react';
-import './css/styles.css'
+import React, { useState, useEffect } from "react";
+import "./css/styles.css";
 
-export default class FetchCasesChina extends React.Component {
-  state = {
-    cases: null,
-    confirmed: null,
-    recovered: null,
-    deaths: null,
-  };
+const FetchCasesChina = () => {
+  const [cases, setCases] = useState(null);
+  const [confirmed, setConfirmed] = useState(null);
+  const [recovered, setRecovered] = useState(null);
+  const [deaths, setDeaths] = useState(null);
 
-  async componentDidMount() {
-    const url = 'https://covid19-brazil-api.now.sh/api/report/v1/china';
-    const response = await fetch(url);
-    const body = await response.json();
-    this.setState({ 
-      cases: body.data.cases,
-      confirmed: body.data.confirmed,
-      recovered: body.data.recovered,
-      deaths: body.data.deaths, 
-    });
-  };
-  render() {
-    return (
-      <div 
-        id="card_china"
-        >
-        <h1>Situação na China</h1>
-        <p 
-          className="mt-4">Casos: 
-          <strong> {this.state.cases} </strong>
-        </p>
-        <p 
-          className="mt-2">Casos confirmados: 
-          <span
-            style={{color: '#FFAA00'}}> {this.state.confirmed}
-          </span>
-        </p>    
-        <p 
-          className="mt-2">Casos recuperados: 
-          <span 
-            style={{color: '#00FF66'}}> {this.state.recovered}
-          </span>
-        </p>
-        <p 
-          className="mt-2">Óbitos confirmados: 
-          <span 
-            style={{color: '#FF0000'}}> {this.state.deaths}
-          </span>
-        </p>
-      </div>
-    );
-  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://covid19-brazil-api.now.sh/api/report/v1/china"
+      );
+      const json = await response.json();
+      setCases(json.data.cases);
+      setConfirmed(json.data.confirmed);
+      setRecovered(json.data.recovered);
+      setDeaths(json.data.deaths);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div id="card_china">
+      <h1>Situação na China</h1>
+      <p className="mt-4">
+        Casos:
+        <strong> {cases} </strong>
+      </p>
+      <p className="mt-2">
+        Casos confirmados:
+        <span style={{ color: "#FFAA00" }}> {confirmed}</span>
+      </p>
+      <p className="mt-2">
+        Casos recuperados:
+        <span style={{ color: "#00FF66" }}> {recovered}</span>
+      </p>
+      <p className="mt-2">
+        Óbitos confirmados:
+        <span style={{ color: "#FF0000" }}> {deaths}</span>
+      </p>
+    </div>
+  );
 };
 
+export default FetchCasesChina;
